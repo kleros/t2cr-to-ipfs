81d8cb5209067cb1765b8476639b11163e0f1928
mtsalenc <mtsalenc@gmail.com>
2020-10-28 15:10:01 -0300

    feat: prepare for use with cron

---
+++ script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# NVM needs the ability to modify your current shell session's env vars,
+# which is why it's a sourced function
+
+# found in the current user's .bashrc - update [user] below with your user!
+export NVM_DIR="/home/ubuntu/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+
+# uncomment the line below if you need a specific version of node
+# other than the one specified as `default` alias in NVM (optional)
+# nvm use 4 1> /dev/null
+
+echo "Using nvm version $(nvm --version)"
+echo "Using node version $(node --version)"
+$(which node) ~/t2cr-to-ipfs/dist/index.js
