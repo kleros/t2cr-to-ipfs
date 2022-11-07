e1de5acef485c170da92ddeb470beef3990c4fc1
gratestas <stasgrate@gmail.com>
2022-11-07 14:06:16 +0300

    feat: add estuary api request logic

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,4 @@
 export { default as ipfsPublish } from './ipfs-publish'
+export { default as estuaryRequest } from './estuary-api'
 export { default as getAddressesWithBadge } from './get-addresses-with-badge'
 export { default as getTokens } from './get-tokens'
